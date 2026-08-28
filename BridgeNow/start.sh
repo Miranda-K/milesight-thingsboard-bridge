4b6bfdd337a0f3e6afe837bed22666eaaa8b121a
Miranda-K <mirandayfkong@gmail.com>
2026-08-28 10:58:27 -0400

    Updated start.sh to run import of rule chains and dashboards after starting the full stack.

--- BridgeNow/start.sh
+++ BridgeNow/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "Setting up ThingsBoard for the first time..."
 docker compose --profile install run --rm thingsboard-installer
-
+ 
 echo "Starting the full stack..."
 docker compose up -d
+ 
+echo "Importing rule chains & dashboards..."
+docker compose --profile provision run --rm tb-provision
